bf468ff49f7a5d069842cf3f9eb43497320f8a2f
DBL70-01S <mducc31082006@gmail.com>
2026-04-02 10:38:57 +0700

    Add files via upload

---
+++ KiemTraCuoiKy/react-minh-hoa/src/data/data.js
@@ -0,0 +1,7 @@
+export const initialData = [
+  { id: 1, name: "Phan Bình An", email: "anpb@gmail.com", diengia:"Nguyễn Văn A", position: "17 Tây Sơn", checkIn:"5/6/2026" },
+  { id: 2, name: "Nguyễn Văn A", email: "abc@gmail.com", diengia:"Nguyễn Văn A", position: "1 Tây Sơn", checkIn:"5/6/2026" },
+  { id: 3, name: "Phan Bình B", email: "ab@gmail.com", diengia:"Nguyễn Văn B", position: "75 Tây Sơn", checkIn:"5/6/2026" },
+  { id: 4, name: "Phan Bình C", email: "anpcsb@gmail.com", diengia:"Nguyễn Văn C", position: "178 Tây Sơn", checkIn:"5/6/2026" },
+  { id: 5, name: "Phan Bình D", email: "accacxvcfb@gmail.com", diengia:"Nguyễn Văn D", position: "15 Tây Sơn", checkIn:"5/6/2026" },
+];
